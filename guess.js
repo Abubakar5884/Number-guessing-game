04af2c036d449fe2b6ff975241dd1ec9db08a4d9
Abubakar siddique <a3123080192@gmail.com>
2024-04-27 23:58:36 +0500

    created number guessing game

---
+++ guess.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// 1) computer will generate a random number
+// 2) user will input the guessed generated number
+// 3) compare the user input with the computer generated number and show the result
+const randomNumber = Math.floor(Math.random() * 101 + 1);
+// console.log(randomNumber);
+const answer = await inquirer.prompt([{
+        name: "guessedNumber",
+        type: "number",
+        message: "please guess a number between 1-100: ",
+    }]);
+if (answer.guessedNumber === randomNumber) {
+    console.log("You guessed the number correct.");
+}
+else {
+    console.log("You guessed the number wrong.");
+}
